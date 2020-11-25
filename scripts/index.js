4749cf6c5961d2c889f323b76279c693d1f2a667
Nox94 <shkarina.evgenia@yandex.ru>
2020-11-25 14:41:23 +0300

    добавляет доработку мелочей на странице

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
 const ProfileButtonEditNode = document.querySelector('.profile__button-edit');
 const PopupNode = document.querySelector('.popup');
-const PopupClose = document.querySelector('.popup__close');
+const PopupCloseNode = document.querySelector('.popup__close');
 
 
 
 ProfileButtonEditNode.addEventListener('click', handleProfileButtonEditClick);
-PopupClose.addEventListener('click', handlePopupCloseClick);
+PopupCloseNode.addEventListener('click', handlePopupCloseClick);
 
 function handleProfileButtonEditClick() {
   PopupNode.classList.add('popup_opened');
 }
 
 function handlePopupCloseClick() {
-  PopupClose.classList.remove('popup_opened');
+  PopupNode.classList.remove('popup_opened');
 }
