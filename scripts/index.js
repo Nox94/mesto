7322332856913f5701a1c0a62f56b1ee6db0e206
Evgenia Amelina <shkarina.evgenia@yandex.ru>
2020-11-27 17:48:28 +0300

    добавляет данные со страницы в форму (js)

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,40 @@
-const ProfileButtonEditNode = document.querySelector('.profile__button-edit');
-const PopupNode = document.querySelector('.popup');
-const PopupCloseNode = document.querySelector('.popup__close');
-const FormNode = document.querySelector('.popup__form');
-const FormFieldNameNode = document.querySelector('.popup__form-row_type_name');
-const FormFieldAboutNode = document.querySelector('.popup__form-row_type_about');
+let ProfileButtonEdit = document.querySelector('.profile__button-edit'); 
+// присвоила переменной с именем ProfileButtonEditNode значение кнопки редактирования, 
+// выбранное методом querySelector из разметки по классу .profile__button-edit
+let Popup = document.querySelector('.popup');
+let PopupClose = document.querySelector('.popup__close');
+let Form = document.querySelector('.popup__form'); //нашла форму в разметке, она является значением переменной FormNode
+let FormFieldName = document.querySelector('.popup__form-row_type_name');
+let FormFieldAbout = document.querySelector('.popup__form-row_type_about');
+let ProfileHeading = document.querySelector('.profile__heading');
+let ProfileSubheading = document.querySelector('.profile__subheading');
+let PopupSave = document.querySelector('.popup__save');
 
-ProfileButtonEditNode.addEventListener('click', handleProfileButtonEditClick);
-PopupCloseNode.addEventListener('click', handlePopupCloseClick);
-FormNode.addEventListener('submit', FormNodeSubmitHandler);
+ProfileButtonEdit.addEventListener('click', handleProfileButtonEditClick); 
+// вешаю обработчик события на кнопку, чтобы потом добавить реакцию на событие. функция handleProfileButtonEditClick - аргумент? зачем?
+// использую функцию handleProfileButtonEditClick, чтобы добавить модификатор _opened по клику пользователя на кнопку редактирования профиля
+PopupClose.addEventListener('click', handlePopupCloseClick);
+// вешаю обработчик события на кнопку "закрыть", чтобы потом добавить реакцию на событие. функция handlePopupCloseClick
+Form.addEventListener('submit', FormNodeSubmitHandler);
+// вешаю обработчик события на кнопку формы "сохранить", чтобы данные перезаписывались и попап закрывался. функция FormNodeSubmitHandler
 
 
 function handleProfileButtonEditClick() {
-  PopupNode.classList.add('popup_opened');
+  Popup.classList.add('popup_opened');
 }
-
+// использую функцию handleProfileButtonEditClick, чтобы добавить модификатор _opened по клику пользователя на кнопку редактирования профиля
 function handlePopupCloseClick() {
-  PopupNode.classList.remove('popup_opened');
+  Popup.classList.remove('popup_opened');
 }
-
+// использую функцию handlePopupCloseClick, чтобы модальное окно закрывалось при нажатии пользователя на крестик
 function FormNodeSubmitHandler(evt) {
   evt.preventDefault();
 }
+// понадобится для того, чтобы работала кнопка "сохранить"
 
-console.log(FormFieldNameNode.value);
-console.log(FormFieldAboutNode.value);
-
+FormFieldName.value = ProfileHeading.textContent;
+FormFieldAbout.value = ProfileSubheading.textContent;
+// присвоила полям формы значения заголовка и подзаголовка со страницы
 
 
 // Находим форму в DOM
