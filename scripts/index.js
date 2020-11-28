623ccf4835840ebbfc1a77ded178a0b84a5e7d5f
Nox94 <shkarina.evgenia@yandex.ru>
2020-11-29 00:49:10 +0300

    меняет инф на стр и закрывает попап ButtonSave

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-let ProfileButtonEdit = document.querySelector('.profile__button-edit'); 
-// присвоила переменной с именем ProfileButtonEditNode значение кнопки редактирования, 
+let ProfileButtonEdit = document.querySelector('.profile__button-edit');
+// присвоила переменной с именем ProfileButtonEditNode значение кнопки редактирования,
 // выбранное методом querySelector из разметки по классу .profile__button-edit
 let Popup = document.querySelector('.popup');
 let PopupClose = document.querySelector('.popup__close');
@@ -10,54 +10,31 @@ let ProfileHeading = document.querySelector('.profile__heading');
 let ProfileSubheading = document.querySelector('.profile__subheading');
 let PopupSave = document.querySelector('.popup__save');
 
-ProfileButtonEdit.addEventListener('click', handleProfileButtonEditClick); 
+ProfileButtonEdit.addEventListener('click', handleProfileButtonEditClick);
 // вешаю обработчик события на кнопку, чтобы потом добавить реакцию на событие. функция handleProfileButtonEditClick - аргумент? зачем?
 // использую функцию handleProfileButtonEditClick, чтобы добавить модификатор _opened по клику пользователя на кнопку редактирования профиля
 PopupClose.addEventListener('click', handlePopupCloseClick);
 // вешаю обработчик события на кнопку "закрыть", чтобы потом добавить реакцию на событие. функция handlePopupCloseClick
-Form.addEventListener('submit', FormNodeSubmitHandler);
+Form.addEventListener('submit', FormSubmitHandler);
 // вешаю обработчик события на кнопку формы "сохранить", чтобы данные перезаписывались и попап закрывался. функция FormNodeSubmitHandler
-
+FormFieldName.value = ProfileHeading.textContent;
+FormFieldAbout.value = ProfileSubheading.textContent;
+// присвоила полям формы значения заголовка и подзаголовка со страницы
 
 function handleProfileButtonEditClick() {
   Popup.classList.add('popup_opened');
 }
 // использую функцию handleProfileButtonEditClick, чтобы добавить модификатор _opened по клику пользователя на кнопку редактирования профиля
+
 function handlePopupCloseClick() {
   Popup.classList.remove('popup_opened');
 }
 // использую функцию handlePopupCloseClick, чтобы модальное окно закрывалось при нажатии пользователя на крестик
-function FormNodeSubmitHandler(evt) {
+
+function FormSubmitHandler(evt) {
   evt.preventDefault();
+  ProfileHeading.textContent = FormFieldName.value;
+  ProfileSubheading.textContent = FormFieldAbout.value;
+  Popup.classList.remove('popup_opened');
 }
-// понадобится для того, чтобы работала кнопка "сохранить"
-
-FormFieldName.value = ProfileHeading.textContent;
-FormFieldAbout.value = ProfileSubheading.textContent;
-// присвоила полям формы значения заголовка и подзаголовка со страницы
-
-
-// Находим форму в DOM
-// let formElement = // Воспользуйтесь методом querySelector() ЕСТЬ
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-// function formSubmitHandler (evt) {  ЕСТЬ
-    // evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    // let nameInput = // Воспользуйтесь инструментом .querySelector() ЕСТЬ
-    // let jobInput = // Воспользуйтесь инструментом .querySelector()  ЕСТЬ
-
-    // Получите значение полей из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', formSubmitHandler);
+// для того, чтобы работала кнопка "сохранить" и обновлялись данные из формы на странице
