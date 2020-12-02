195c2acb2b9736620241a7b4a405c074c801cc6d
Evgenia Amelina <shkarina.evgenia@yandex.ru>
2020-12-02 10:44:15 +0300

    вносит исправления после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -9,14 +9,10 @@ let formFieldAbout = document.querySelector(".popup__form-row_type_about");
 let profileHeading = document.querySelector(".profile__heading");
 let profileSubheading = document.querySelector(".profile__subheading");
 
-formFieldName.value = profileHeading.textContent;
-formFieldAbout.value = profileSubheading.textContent;
-// присвоила полям формы значения заголовка и подзаголовка со страницы
-
 function handleProfileButtonEditClick() {
-  popup.classList.add("popup_opened");
   formFieldName.value = profileHeading.textContent;
   formFieldAbout.value = profileSubheading.textContent;
+  popup.classList.add("popup_opened");
   // присвоила полям формы значения заголовка и подзаголовка со страницы и поместила их в функцию, чтобы значения в поля загружались при каждом нажатии на кнопку.
 }
 // использую функцию handleProfileButtonEditClick, чтобы добавить модификатор _opened по клику пользователя на кнопку редактирования профиля
