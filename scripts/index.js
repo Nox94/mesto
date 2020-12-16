a87026306cb82be2582161797839d618eb0e7445
Nox94 <shkarina.evgenia@yandex.ru>
2020-12-16 11:07:32 +0300

    делает загрузку первых шести карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -16,18 +16,53 @@ const buttonAdd = document.querySelector(".profile__button-add");
 const cardHeading = document.querySelector(".elements__card-heading");
 const cardImage = document.querySelector(".elements__card-img");
 const cardsContainer = document.querySelector(".elements");
-
 //получила шаблон карточки себе в переменную вместе с содержимым
+
+
 //работа с карточками
+//создала массив с данными карточек - заголовок и изображение (и зачем???)
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "./images/arkhyz.jpg",
+  },
+  {
+    name: "Эльбрус",
+    link: "./images/elbrus.jpg",
+  },
+  {
+    name: "Карачаевск",
+    link: "./images/karachaevsk.jpg",
+  },
+  {
+    name: "Пятигорск",
+    link: "./images/pyatigorsk.jpg",
+  },
+  {
+    name: "Сочи",
+    link: "./images/sochi.jpg",
+  },
+  {
+    name: "Архыз",
+    link: "./images/arkhyz.jpg",
+  },
+];
+
+initialCards.forEach(function (item) {
+  let some = item.name;
+  let somethingElse = item.link;
+  addCard(some, somethingElse);
+});
+
 
 // рендер массива для добавления первых шести карточек на страницу при загрузке
 function renderList() {
   const listItems = initialCards.map(addCard);
-  console.log(listItems);
-  cardsContainer.append(...cardElement);
-}
-renderList();
-//ф-ция создания и добавления (?) карточки
+cardsContainer.append(...initialCards);
+  };
+
+
+//ф-ция создания и добавления карточки
 function addCard(cardHeading, cardImage) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.cloneNode(true);
