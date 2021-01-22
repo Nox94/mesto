15df2530e589bc89b1f17f18f140992f03edec3c
Evgenia Amelina <shkarina.evgenia@yandex.ru>
2021-01-22 14:43:02 +0300

    работает над классом FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,13 +1,12 @@
 export class FormValidator {
   constructor(validationConfig, formSelector) {
-    const validationConfig = {
-      this._formSelector: '.popup__form',
-      this._inputSelector: '.popup__form-row',
-      this._submitButtonSelector: '.popup__save',
-      this._inputInvalidClass: 'popup__form-row_state_invalid',
-      this._buttonInvalidClass: 'popup__save_invalid',
-  }
-  }
+      this._formSelector = '.popup__form',
+      this._inputSelector = '.popup__form-row',
+      this._submitButtonSelector = '.popup__save',
+      this._inputInvalidClass = 'popup__form-row_state_invalid',
+      this._buttonInvalidClass = 'popup__save_invalid'
+    }
+
   _checkInputValidity(form, input, config) { //проверка валидации полей формы (приватный)
     if (!input.validity.valid) {
       showError(form, input, config);
@@ -17,16 +16,16 @@ export class FormValidator {
   }
   _setButtonState(button, isActive, config) { //проверка состояния кнопки сабмита (приватный)
     if (isActive) {
-      button.classList.remove(config.buttonInvalidClass);
+      button.classList.remove(config._buttonInvalidClass);
       button.disabled = false;
     } else {
-      button.classList.add(config.buttonInvalidClass);
+      button.classList.add(config._buttonInvalidClass);
       button.disabled = true;
     }
   }
   _setEventListeners(form, config) { //обработчики событий по клику (приватный)
-    const inputsList = form.querySelectorAll(config.inputSelector);
-    const submitButton = form.querySelector(config.submitButtonSelector);
+    const inputsList = form.querySelectorAll(config._inputSelector);
+    const submitButton = form.querySelector(config._submitButtonSelector);
     inputsList.forEach((input) => {
         input.addEventListener('input', () => {
             checkInputValidity(form, input, config);
@@ -37,28 +36,27 @@ export class FormValidator {
   _showError(form, input, config) { //показать ошибку (приватный)
   const error = form.querySelector(`#${input.id}-error`);
   error.textContent = input.validationMessage;
-  input.classList.add(config.inputInvalidClass);
+  input.classList.add(config._inputInvalidClass);
 }
   _hideError(form, input, config) { //скрыть ошибку (приватный)
   const error = form.querySelector(`#${input.id}-error`);
   error.textContent = '';
-  input.classList.remove(config.inputInvalidClass);
+  input.classList.remove(config._inputInvalidClass);
 }
 
   enableValidation(config) { //запуск валидации (публичный)
-  const forms = document.querySelectorAll(config.formSelector);
+  const forms = document.querySelectorAll(config._formSelector);
   forms.forEach((form) => {
       setEventListeners(form, config);
       form.addEventListener('submit', (evt) => {
           evt.preventDefault();
       });
-      const submitButton = form.querySelector(config.submitButtonSelector);
+      const submitButton = form.querySelector(config._submitButtonSelector);
       setButtonState(submitButton, form.checkValidity(), config)
   });
 }
   enableValidation(validationConfig);
 }
-
 
 new FormValidator(validationConfig, popupProfileSave);
 new FormValidator(validationConfig, popupCardElemSave);
