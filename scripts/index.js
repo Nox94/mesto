0d89c8f24fb24ac36e794962f883a8e9a08e9270
Nox <Nox@gmail.com>
2021-01-27 17:34:00 +0300

    working on card class in process

--- scripts/index.js
+++ scripts/index.js
@@ -26,12 +26,22 @@ const validationConfig = {
   buttonInvalidClass: 'popup__submit_invalid',
 };
 
+const cardConfigurator = {
+  template: cardTemplate,
+  image: ".elements__card-img",
+  heading: ".elements__card-heading",
+  deleteButton: ".elements__remove-button",
+  likeButton: ".elements__like-button",
+  name: '',
+  link: '', 
+};
+
 const editProfileFormValid = new FormValidator(validationConfig, popupProfileSave);
 const addCardFormValid = new FormValidator(validationConfig, popupCardElemSave);
 editProfileFormValid.enableValidation();
 addCardFormValid.enableValidation();
 
-const newCard = new Card(initialCards, );
+
 
 //работа с карточками
 //создала массив с данными карточек - заголовок и изображение
@@ -63,9 +73,10 @@ export const initialCards = [
 ];
 
 initialCards.forEach((item) => {
-  const cardText = item.name;
-  const cardImageLink = item.link;
-  addCard(cardsContainer, createCard(cardText, cardImageLink, cardTemplate));
+  cardConfigurator.name = item.name;
+  cardConfigurator.link = item.link;
+  const newCard = new Card(cardConfigurator, openPopupPictureHandler);
+  addCard(cardsContainer, newCard.createCard());
 });
 
 //добавление карточки
@@ -85,6 +96,15 @@ function popupEscapeHandler(evt) {
     const active = evt.currentTarget.querySelector('.popup_opened');
     closeAnyPopup(active);
   }
+}
+
+function openPopupPictureHandler(event) {
+  //попап карточки открывается по нажатию на картинку карточки
+  popupPicture.src = event.target.src; // event.target является тем объектом, на который произвели клик, в данном случае это картинка карточки, она присваивается как значение картинки попапа
+  popupPicture.alt = event.target.alt; //добавляет атрибут alt изображению попапа картинки карточки по названию карточки
+  const parentSearching = event.target.closest(".elements__card"); //находим в DOM родительский контейнер картинки — див со всеми элементами карточки и присваиваем его переменной
+  popupCapture.textContent = parentSearching.querySelector(".elements__card-heading").textContent; //берем текстовое содержимое переменной, а которой хранится подпись к картинке попапа и присваиваем ей значение заголовка карточки через поиск в родительском элементе — родителе карточки с помощью метода querySelector
+  openPopup(popupImage); //делаем попап видимым после того, как он получил в себя все данные
 }
 
 // функция открытия попапов
